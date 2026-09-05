8f794f7415507eb28b07445bb3a1764336cfdc48
Jacob Otero <jacobotero0313@gmail.com>
2026-09-05 16:42:22 -0500

    Add the S3+CloudFront frontend stack

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -1,7 +1,44 @@
-from aws_cdk import Stack
+from aws_cdk import Stack, RemovalPolicy, Duration
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_cloudfront as cloudfront
+from aws_cdk import aws_cloudfront_origins as origins
 from constructs import Construct
 
 
 class DonortrackStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        self.frontend_bucket = s3.Bucket(
+            self,
+            "FrontendBucket",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+
+        self.distribution = cloudfront.Distribution(
+            self,
+            "Distribution",
+            default_root_object="index.html",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3BucketOrigin.with_origin_access_control(
+                    self.frontend_bucket
+                ),
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            ),
+            error_responses=[
+                cloudfront.ErrorResponse(
+                    http_status=403,
+                    response_http_status=200,
+                    response_page_path="/index.html",
+                    ttl=Duration.seconds(0),
+                ),
+                cloudfront.ErrorResponse(
+                    http_status=404,
+                    response_http_status=200,
+                    response_page_path="/index.html",
+                    ttl=Duration.seconds(0),
+                ),
+            ],
+        )
--- infra/tests/unit/test_infra_stack.py
+++ infra/tests/unit/test_infra_stack.py
@@ -9,3 +9,27 @@ def test_stack_synthesizes():
     stack = DonortrackStack(app, "TestStack")
     template = Template.from_stack(stack)
     assert template is not None
+
+
+def test_frontend_bucket_blocks_public_access():
+    app = cdk.App()
+    stack = DonortrackStack(app, "TestStack")
+    template = Template.from_stack(stack)
+    template.has_resource_properties(
+        "AWS::S3::Bucket",
+        {
+            "PublicAccessBlockConfiguration": {
+                "BlockPublicAcls": True,
+                "BlockPublicPolicy": True,
+                "IgnorePublicAcls": True,
+                "RestrictPublicBuckets": True,
+            }
+        },
+    )
+
+
+def test_cloudfront_distribution_exists():
+    app = cdk.App()
+    stack = DonortrackStack(app, "TestStack")
+    template = Template.from_stack(stack)
+    template.resource_count_is("AWS::CloudFront::Distribution", 1)
